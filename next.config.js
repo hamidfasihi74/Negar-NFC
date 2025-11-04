c2d4bd4f6da796b7f3170e0b10e91608b97283f9
hamidfasihi74 <hamidfasihi74@gmail.com>
2025-11-04 11:37:01 +0430

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['fa', 'ps', 'en'],
+    defaultLocale: 'fa',
+  },
+}
+
+module.exports = nextConfig
